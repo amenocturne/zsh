c9af92b83ae654966ced21bd4121c73f7a7d3cf9
skril <skril@skrils-MacBook-Air.local>
2026-01-03 17:12:18 +0300

    improve history: immediate write, timestamps, larger size

--- usr/history.zsh
+++ usr/history.zsh
@@ -1,11 +1,12 @@
 # History
-HISTSIZE=10000
-SAVEHIST=10000
+HISTSIZE=50000
+SAVEHIST=50000
 HISTFILE="$HOME/.config/zsh/history"
-setopt appendhistory
+
+setopt INC_APPEND_HISTORY     # Write immediately, not on exit
+setopt EXTENDED_HISTORY       # Save timestamps
 setopt HIST_EXPIRE_DUPS_FIRST
-setopt HIST_IGNORE_DUPS
-setopt HIST_IGNORE_ALL_DUPS
-setopt HIST_IGNORE_SPACE
+setopt HIST_IGNORE_ALL_DUPS   # Remove older duplicate
+setopt HIST_IGNORE_SPACE      # Commands starting with space are private
 setopt HIST_FIND_NO_DUPS
 setopt HIST_SAVE_NO_DUPS
